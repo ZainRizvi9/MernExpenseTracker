4b3a8e80977475176d06e13c37d2fb1972fb01a7
ZainRizvi9 <rizvi-zain@hotmail.com>
2025-10-26 19:56:31 -0400

    UserContext uppercase

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -5,7 +5,7 @@ import Input from '../../components/Inputs/Input';
 import { validateEmail } from '../../utils/helper'; 
 import axiosInstance from "../../utils/axiosInstance"
 import { API_PATHS } from "../../utils/apiPaths"
-import { UserContext } from '../../context/userContext.jsx';
+import { UserContext } from '../../context/UserContext.jsx';
 
 const Login = () => {
     const [email, setEmail] = useState("");
