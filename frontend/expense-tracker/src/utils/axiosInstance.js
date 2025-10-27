cd1cbff21165c1d7150f84d4b97faadb83fd4d09
ZainRizvi9 <rizvi-zain@hotmail.com>
2025-10-27 15:04:36 -0400

    Update axiosInstance.js

--- frontend/expense-tracker/src/utils/axiosInstance.js
+++ frontend/expense-tracker/src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ const BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 
 const axiosInstance = axios.create({
-  baseURL: `${BASE_URL}/api/v1`,
+  baseURL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
